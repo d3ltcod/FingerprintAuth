98fd96713722a0925e2cf4973bf341310dd6d363
D3ltcod <d3ltcod@protonmail.com>
2018-01-03 18:05:16 +0100

    Created generateKey method

--- app/src/main/java/com/d3ltcod/fingerprintauth/MainActivity.java
+++ app/src/main/java/com/d3ltcod/fingerprintauth/MainActivity.java
@@ -5,10 +5,22 @@ import android.app.KeyguardManager;
 import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.KeyGenerator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private FingerprintManager fingerprintManager;
     private KeyguardManager keyguardManager;
+    private KeyStore keyStore;
+    private KeyGenerator keyGenerator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +71,48 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 //TODO
             }
+        }
+    }
+
+    //Create the generateKey method that we’ll use to gain access to the Android keystore and generate the encryption key
+
+    private void generateKey() throws RuntimeException {
+        try {
+            // Obtain a reference to the Keystore using the standard Android keystore container identifier (“AndroidKeystore”)
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+
+            //Generate the key
+            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+
+            //Initialize an empty KeyStore
+            keyStore.load(null);
+
+            //Initialize the KeyGenerator
+            keyGenerator.init(new
+
+                    //Specify the operation(s) this key can be used for
+                    KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+
+                    //Configure this key so that the user has to confirm their identity with a fingerprint each time they want to use it
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+
+            //Generate the key
+            keyGenerator.generateKey();
+
+        } catch (KeyStoreException
+                | NoSuchAlgorithmException
+                | NoSuchProviderException
+                | InvalidAlgorithmParameterException
+                | CertificateException
+                | IOException exc) {
+            exc.printStackTrace();
+            throw new RuntimeException(exc);
         }
     }
 }
