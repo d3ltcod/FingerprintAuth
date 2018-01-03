789ccd479db4b80ba15f7edac643c2084fbaa65a
D3ltcod <d3ltcod@protonmail.com>
2018-01-03 17:31:40 +0100

    Added code to check initial state of fingerprint sensor.

--- app/src/main/java/com/d3ltcod/fingerprintauth/MainActivity.java
+++ app/src/main/java/com/d3ltcod/fingerprintauth/MainActivity.java
@@ -1,13 +1,64 @@
 package com.d3ltcod.fingerprintauth;
 
+import android.Manifest;
+import android.app.KeyguardManager;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // String variable for the key that we are going to use in our fingerprint auth
+    private static final String KEY_NAME = "fingerprintKey";
+    private TextView textView;
+    private FingerprintManager fingerprintManager;
+    private KeyguardManager keyguardManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // We need to verify that the device is running Marshmallow or higher before executing any fingerprint-related code
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //Get an instance of KeyguardManager and FingerprintManager//
+            keyguardManager =
+                    (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+            fingerprintManager =
+                    (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+
+            textView = (TextView) findViewById(R.id.textview);
+
+            //Check if device has a fingerprint sensor.
+            if (!fingerprintManager.isHardwareDetected()) {
+                // If a fingerprint sensor isn’t available, then inform the user.
+                textView.setText("Your device doesn't support fingerprint authentication");
+            }
+            //Check if user has granted your app the USE_FINGERPRINT permission
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+                // If your app doesn't have this permission, then display information message.
+                textView.setText("Please enable the fingerprint permission");
+            }
+
+            //Check if user has registered at least one fingerprint
+            if (!fingerprintManager.hasEnrolledFingerprints()) {
+                // If the user hasn’t configured any fingerprints, then display the following message.
+                textView.setText("No fingerprint configured. Please register at least one fingerprint in your device's Settings");
+            }
+
+            //Check that the lockscreen is secured
+            if (!keyguardManager.isKeyguardSecure()) {
+                // If the user hasn’t secured their lockscreen with a PIN password or pattern, then display the following text.
+                textView.setText("Please enable lockscreen security in your device's Settings");
+            } else {
+                //TODO
+            }
+        }
+    }
+                }
     }
 }
